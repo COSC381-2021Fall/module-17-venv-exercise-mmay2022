5120d2a08d6b33fc1c2866134619815a93b0343f
Michael May <mmay14@emich.edu>
2021-11-07 21:37:23 +0000

    Added py file using packages

---
+++ PyProject/library_test.py
@@ -0,0 +1,14 @@
+from typing import Generator
+import arrow
+from parse import *
+from joke_generator import generate
+
+print(generate())
+print(parse("Hello {}", "Hello World"))
+utc = arrow.utcnow()
+print(utc)
+
+
+
+
+
